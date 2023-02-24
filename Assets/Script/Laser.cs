d3ac321fa2b277668983ee2eae2244a1c8863c2d
AkarisuMD <91060309+AkarisuMD@users.noreply.github.com>
2023-02-24 11:44:17 +0100

    zjvbsufs

--- Assets/Script/Laser.cs
+++ Assets/Script/Laser.cs
@@ -15,14 +15,14 @@ public class Laser : MonoBehaviour
 
     private void FixedUpdate()
     {
-        lr.gameObject.transform.position= Vector3.zero;
+        lr.gameObject.transform.localPosition= Vector3.zero;
         lr.SetPosition(0, Vector3.zero);
+        print(lr.gameObject.transform.position);
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(Emitter.transform.position, Emitter.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
         {
             lr.SetPosition(1, hit.collider.gameObject.transform.localPosition);
-            print(hit.collider.gameObject.transform.position);
             Debug.DrawRay(Emitter.transform.position, Emitter.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
            
 
